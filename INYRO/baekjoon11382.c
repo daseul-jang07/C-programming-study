2ffa66081d823a02cafcb6d5723d3606fe87e0fd
daseul-jang07 <daseuljang24@gmail.com>
2025-10-02 12:03:12 +0900

    baekjoon update

---
+++ INYRO/baekjoon11382.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long long A;
+    long long B;
+    long long C;
+    
+    scanf("%lld %lld %lld", &A, &B, &C);
+
+    printf("%lld", A + B + C);
+
+    return 0;
+}
